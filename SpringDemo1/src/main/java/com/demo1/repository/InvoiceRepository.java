05b1fda341013a07907ed899b13003cd5ede6245
Jyoti Kulbhau <jyoti.kulbhau@viraltech.in>
2025-01-30 11:33:09 +0530

    Removed all files

--- SpringDemo1/src/main/java/com/demo1/repository/InvoiceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.demo1.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.demo1.model.Invoice;
-
-public interface InvoiceRepository extends JpaRepository<Invoice,Long>{
-
-}
--- SpringDemoApp1/src/main/java/com/jwt/service/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.jwt.service;
-
-public class ProductNotFoundException extends RuntimeException{
-
-	private static final long serialVersionUID = 1L;
-
-}
